4a391822cc366f4f9f749e43941a922a82b45f4f
Oleg <oleglyash@mail.ru>
2013-12-23 18:46:25 +0400

    ...

--- start.py
+++ start.py
@@ -21,22 +21,50 @@ except ImportError, err:
     print ERROR_LOADING_MODULES+" %s" % (err)
     sys.exit(2)
 
-    
-##class Balloon(pygame.sprite.Sprite):
-##    ''' Воздушный шарик, кторый движется снизу вверх
-##        Возвращает: объект Balloon
-##        Функции: update, calcnewpos
-##        Атрибуты: area, vector'''
-##    def __init__(self, (xy), vector):
-##        pygame.sprite.Sprite.__init__(self)
-##        self.image, self.rect = load_png('balloon01.png')
-##        screen = pygame.display.get_surface()
-##        self.area = screen.get_rect()
-##        self.vector = vector
-##        self.hit = 0    
-        
+
+def load_img(name):
+    '''
+    Функция load_img загружает изображение
+    Принимает name - имя файла
+    Возвращает объект-изображение
+    '''
+    try:
+        image = pygame.image.load(name)
+        if image.get_alpha is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error, message:
+        print ERROR_LOADING_IMAGE, name
+        raise SystemExit, message
+    return image, image.get_rect()
+
+class Balloon(pygame.sprite.Sprite):
+    '''
+        Воздушный шарик, кторый движется снизу вверх
+        Возвращает: объект Balloon
+        Функции:
+        Атрибуты:
+    '''
+    def __init__(self,path,name):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png(os.path.join(self.path+"/img/", name))
+        self.screen = pygame.display.get_surface()
+        self.alive=True
+        self.X=0
+        self.Y=0
+        self.boom=False
+    def renew(self):
+        pass
+    def move(self):
+        pass
+    def draw(self):
+        pass
+        self.screen
+
+
 class Game():
-    ''' 
+    '''
     Класс Game формирует игру
     Метод _init_ принимает на вход логическую переменную, которая задает режим отображения (полноэкранный или оконный)
     '''
@@ -45,7 +73,7 @@ class Game():
         Инициализация игры
         '''
         pygame.init()
-        
+
         print(CREATE_GAME_CLASS)
         self.path="data"
         self.done=False
@@ -60,21 +88,23 @@ class Game():
         self.score_step_next=100 #на сколько следует увеличить планку перехода на следующий уровень
         self.life=5 #кол-во жизней
         self.count=3 #кол-во шариков, которые видны на экране
-        self.speed=15 #скокрость движения шариков
+        self.speed=5 #скокрость движения шариков
         #Оставаться в цикле, пока пользователь не нажмёт на кнопку закрытия окна
         self.menu=True
         self.earthquake=0 # Дрожжание экрана
-        self.earthquake_time=0 #Время в течении которого экран дрожжит        
+        self.earthquake_time=0 #Время в течении которого экран дрожжит
+        self.cursorX=0
+        self.cursorY=0
 
         # Используется для контроля частоты обновления экрана
-        self.clock=pygame.time.Clock()   
+        self.clock=pygame.time.Clock()
         pygame.font.init()
 
         font = pygame.font.Font(None, 25) #для вывода игровой информации
         font2 = pygame.font.Font(None, 80) #для вывода крупных надписей
         #Прячем курсор
-        pygame.mouse.set_visible(0)
-        
+        #pygame.mouse.set_visible(0)
+
         if Fullscreen:
             print(FULLSCREEN_ENABLE)
             self.size=[pygame.display.list_modes()[0][0],pygame.display.list_modes()[0][1]]
@@ -82,7 +112,7 @@ class Game():
         else:
             print(FULLSCREEN_DISABLE)
             self.size=[800,600]
-            
+
             #Получаем размеры экрана
             scr_width=pygame.display.Info().current_w
             scr_height=pygame.display.Info().current_h
@@ -93,12 +123,12 @@ class Game():
             self.screen=pygame.display.set_mode(self.size)
             pygame.display.set_caption(GAME_CAPTION)
             #Получаем объект-экран
-            self.screen=pygame.display.get_surface() 
+            self.screen=pygame.display.get_surface()
             self.load_data()
 
 
         #Создаем список с информацией о шариках
-        #координаты, картинка, скорость.           
+        #координаты, картинка, скорость.
         #координаты, картинка, скорость.
         #img,x,y,speed
         self.balloons=[]
@@ -108,7 +138,7 @@ class Game():
                             random.randrange(self.size[1]),
                             random.randint(self.speed//1.5,self.speed)]
                             )
-            
+
 
 
         self.main_loop()
@@ -152,7 +182,7 @@ class Game():
             self.done=True
             raise SystemExit, message
 
-        
+
 
         #Загружаем музыку
         print("трек Johaness Gilther - Subcarpathia (Original Mix)Album artwork")
@@ -181,14 +211,18 @@ class Game():
             raise SystemExit, message
 
     def draw_npc(self):
-        ''' Метод draw_npc обрабатывает события		для игровых объектов и выполняет их отрисовку на		холсте '''
-        self.screen.blit(self.balloons_img[0][0],[100,100])
-    
+        ''' Метод draw_npc обрабатывает события     для игровых объектов и выполняет их отрисовку на        холсте '''
+        for i in self.balloons:
+            i[2]-=i[3]
+            if i[2]<0:
+                i[2]=self.size[1]
+            self.screen.blit(self.balloons_img[i[0]][0],[i[1],i[2]])
+
     def draw_player(self):
         '''
         Метод draw_player обрабатывает события для игрока и выполняет его отрисовку на холсте
-        '''		
-        self.screen.blit(self.cursor[0],[200,100])
+        '''
+        self.screen.blit(self.cursor[0],[self.cursorX,self.cursorY])
 
 
     def draw_scene(self):
@@ -202,6 +236,10 @@ class Game():
     def events(self):
         '''Обработка событий
         '''
+        pos = pygame.mouse.get_pos()
+        self.cursorX=pos[0]
+        self.cursorY=pos[1]
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #Если пользователь нажал кнопку закрытия окна
                 print(GOOD_BY)
@@ -232,10 +270,10 @@ class Game():
             pygame.display.flip()
         #Выход из программы
         pygame.quit()
-		    
+
 def main():
     #True - включить полноэкранный режим
     #False - выключить полноэкранный режим
-    BallonsGame = Game(False)	
-	
+    BallonsGame = Game(False)
+
 main()
